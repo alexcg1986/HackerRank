09c366a5397c0c672d548509e7a95ba07d7fb836
Mezdelex <alexcondegomez@gmail.com>
2023-08-01 08:41:42 +0200

    added Hello World Haskell solution

---
+++ Haskell/HelloWorld/HelloWorld.hs
@@ -0,0 +1,16 @@
+-- A "Hello World" program has been widely adopted as the introductory program for learning how to code. So, let's start functional programming by writing the same program.
+
+-- Sample Input
+-- (Nil)
+
+-- Sample Output
+-- Hello World
+
+-- Method Signature
+-- Number Of Parameters: 0
+-- Parameters: []
+-- Returns: nil
+-- Behavior: Prints "Hello World"
+
+main :: IO ()
+main = putStr "Hello World"
